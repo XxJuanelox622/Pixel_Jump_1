40c66e689813b99ecf0c902d14e2c686f0d3520e
Juan Carlos <juan.carlos622ibarra@gmail.com>
2025-06-30 04:05:54 -0600

    Con_niv_VF

--- contador.gd
+++ contador.gd
@@ -2,11 +2,8 @@ extends Node
 
 var puntuacion = 0
 
-@onready var etiqueta_puntuacion: Label = $EtiquetaPuntuacion
-
 signal puntuacion_actualizada(puntuacion_actual: int)
 
 func incrementa_un_punto():
 	puntuacion += 1
 	puntuacion_actualizada.emit(puntuacion)
-	etiqueta_puntuacion.text = "Has obtenido\n"+str(puntuacion)+"monedas."
